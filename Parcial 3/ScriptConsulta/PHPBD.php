d7b5fff973b24b4b55b5c6a2e36d7f2175d2d1fc
RevisOTO <rafamosqueda.barragan12@gmail.com>
2022-05-25 17:00:05 -0600

    Script Consulta Fix

--- Parcial 3/ScriptConsulta/PHPBD.php
+++ Parcial 3/ScriptConsulta/PHPBD.php
@@ -1,8 +1,10 @@
 <?php
         $mbd = new PDO('mysql:host=localhost;dbname=L19100223', 'root', '');
-        $consulta = 'SELECT * from Formulario where idPersona = 2';
+        $consulta = 'SELECT * from Formulario where idPersona = 1';
         $rows = $mbd->prepare($consulta);
         $rows -> execute();
         $res = $rows->fetch(pdo::FETCH_ASSOC);
-        echo json_encode($res);
+
+        $res_utf8 = array_map('utf8_encode', $res);
+        echo json_encode($res_utf8);
 ?>
